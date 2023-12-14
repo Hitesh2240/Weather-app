e2b15212ce301dcdb49a6a3458006fb91b0bba2f
Hitesh2240 <hiteshsharma5098@gmail.com>
2023-12-14 16:38:49 +0530

    some changes

--- src/Component/Weather.jsx
+++ src/Component/Weather.jsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ColorRing } from "react-loader-spinner";
 import styles from "../styles/weather.module.css";
 
 const WeatherNewComp = () => {
   const [cityName, setCityName] = useState("");
-
   const [weatherData, setWeatherData] = useState();
   const [error, setError] = useState();
   const [loading, setLoading] = useState(false);
@@ -13,8 +12,6 @@ const WeatherNewComp = () => {
     try {
       setLoading(true);
       let API_KEY = 'cf1a5d649b6ffe51e14f6b31aab2dbe8';
-      console.log(API_KEY);
-      // throw new Error ("Testing Error")
       let response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
       );
@@ -22,14 +19,13 @@ const WeatherNewComp = () => {
       console.log(result);
       if (result.cod == "404") {
         setError(result.message);
-        console.log("error occured");
+        console.log("error occurred");
       }
       if (result.cod !== "400" && result.cod !== "404") {
         setWeatherData(result);
         setError("");
       }
     } catch (error) {
-      // setError("An Error occured while fetching request")
       console.log(error);
       setError(error.message);
     } finally {
@@ -37,51 +33,54 @@ const WeatherNewComp = () => {
     }
   }
 
-  useEffect(() => {
+  const handleSearch = () => {
     getWeatherData();
-  }, [cityName]);
+  };
 
-  console.log(cityName);
-  // getWeatherData()
+  const handleInputChange = (e) => {
+    setCityName(e.target.value);
+  };
+
   function convertToCelcius(temp) {
     let newTemp = temp - 273;
     return Math.floor(newTemp);
   }
+
   return (
     <>
       <div className={styles.main}>
         <div className={styles.container}>
           <h1 className={styles.heading}>Weather App</h1>
-        
-        <input
-            type="text"
-            value={cityName}
-            onChange={(e) => setCityName(e.target.value)}
-            className={styles.search}
-            placeholder="Enter the City"
-          />
-          
-            {loading && (
-              <ColorRing
-                visible={true}
-                height="70"
-                width="70"
-                ariaLabel="blocks-loading"
-                wrapperStyle={{}}
-                wrapperClass="blocks-wrapper"
-                colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
-              />
-            )}
-        
+          <div className={styles.searchContainer}>
+            <input
+              type="text"
+              value={cityName}
+              onChange={handleInputChange}
+              className={styles.search}
+              placeholder="Enter the City"
+            />
+            <button onClick={handleSearch} className={styles.searchButton}>
+              Search
+            </button>
+          </div>
+          {loading && (
+            <ColorRing
+              visible={true}
+              height="70"
+              width="70"
+              ariaLabel="blocks-loading"
+              wrapperStyle={{}}
+              wrapperClass="blocks-wrapper"
+              colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+            />
+          )}
           {error && <p style={{ color: "red" }}>Error: {error}</p>}
-
           {weatherData && (
             <div className={styles.box}>
               <h3 className={styles.country}>
                 {weatherData.name} ({weatherData?.sys?.country}){" "}
               </h3>
               <h3 className={styles.weatherType}>
-                {" "}
                 {weatherData.weather &&
                   weatherData?.weather[0]?.description}{" "}
               </h3>
@@ -103,4 +102,5 @@ const WeatherNewComp = () => {
     </>
   );
 };
+
 export default WeatherNewComp;
